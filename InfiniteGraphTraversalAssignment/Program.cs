0a005e950e4e560842d014b20e208450e913197e
Dreams101010 <m0chk0z@gmail.com>
2019-12-12 05:05:11 +0300

    Initial commit

---
+++ InfiniteGraphTraversalAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InfiniteGraphTraversalAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RadoGraph g = new RadoGraph();
+            AdjacencyMatrixGraph g = new AdjacencyMatrixGraph(6);
+            g.SetEdge(0, 1);
+            g.SetEdge(0, 4);
+            g.SetEdge(1, 2);
+            g.SetEdge(1, 3);
+            g.SetEdge(3, 4);
+            g.SetEdge(3, 5);
+            var trav = g.GetTraversalEnumerator(TraversalStrategyEnum.DFS);
+            trav.OnAllChildrenVisited += delegate(int node)
+            {
+                Console.WriteLine($"Node {node}'s children had been visited.");
+            };
+            trav.OnHasNoChildren += delegate (int node)
+            {
+                Console.WriteLine($"Node {node} has no children.");
+            };
+            while (trav.MoveNext())
+            {
+                Console.WriteLine(trav.Current);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ InfiniteGraphTraversalAssignment/TraversalStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// TODO: event clean-up
+
+namespace InfiniteGraphTraversalAssignment
+{
+    abstract class TraversalStrategy
+    {
+        protected Graph graph = null;
+
+        public delegate void AllChildrenVisitedHandler(int nodeIndex);
+        public virtual event AllChildrenVisitedHandler OnAllChildrenVisited;
+
+        public delegate void HasNoChildrenHandler(int nodeIndex);
+        public virtual event HasNoChildrenHandler OnHasNoChildren;
+
+        public abstract bool IsFinished { get; protected set; }
+        public abstract int Execute();
+    }
+}
